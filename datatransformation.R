7cf0012aeb35901ba3cb190518b7fc3cacbd0f01
ashutoshm90 <ashutoshm90@gmail.com>
2018-09-30 12:58:09 +0530

    graph

--- datatransformation.R
+++ datatransformation.R
@@ -9,3 +9,13 @@ count(arrange(filter(mtcars, gear==4), wt))
 summarise(group_by(mtcars,am), mean(mpg))
 summarise(group_by(mtcars,gear), mean(wt))
 filter(mtcars, between(row_number(),5,n()-2))
+mutate(mtcars,disp / 61.0237)
+transmute(mtcars,disp/40)
+slice(mtcars, 2)
+?slice
+
+slice(mtcars, 3:6)
+summarise(group_by(mtcars, cyl),mean(disp),sd(disp))
+summarise_all(group_by(mtcars, cyl),mean)
+sample_frac(mtcars,.2,replace=F)
+sample_n(mtcars,2,replace=F)
